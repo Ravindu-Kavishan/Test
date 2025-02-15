f9a3efb83d2645ceb52d028df75cf58215d9c3a9
Ravindu-Kavishan <Priyadarshandmrk.22@uom.lk>
2025-02-15 16:08:06 +0530

    Third commit

--- main.py
+++ main.py
@@ -5,15 +5,17 @@ app = FastAPI()
 
 class MessageRequest(BaseModel):
     git_diff: str
-    commit_type:str
+    commit_type: str
 
 @app.get("/")
 def read_root():
     return {"message": "API is running"}
 
+@app.head("/")
+def head_root():
+    return {"message": "API is running"}
+
 @app.post("/generateCommit")
 def process_message(request: MessageRequest):
-    # print(request.message)
-    # LLM function
-    new_message = "first comit"
+    new_message = "first commit"
     return {"new_message": new_message}
