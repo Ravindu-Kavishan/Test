e545d348bb59aec1fe5860bac328c17d771d959b
Ravindu-Kavishan <Priyadarshandmrk.22@uom.lk>
2025-02-15 15:38:58 +0530

    First commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class MessageRequest(BaseModel):
+    git_diff: str
+    commit_type:str
+
+@app.post("/generateCommit")
+def process_message(request: MessageRequest):
+    # print(request.message)
+    # LLM function
+    new_message = "first comit"
+    return {"new_message": new_message}
